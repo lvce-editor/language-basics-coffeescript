296877bfef66eaa058e2a88216bb826f0e0eea65
Le Vivilet <levivlet@pm.me>
2023-01-04 14:17:44 +0100

    fix: highlighting of comments

--- src/tokenizeCoffeeScript.js
+++ src/tokenizeCoffeeScript.js
@@ -1,13 +1,50 @@
 /**
  * @enum number
  */
+export const State = {
+  TopLevelContent: 1,
+  InsideDoubleQuoteString: 2,
+}
+
+export const StateMap = {
+  [State.TopLevelContent]: 'TopLevelContent',
+}
+
+/**
+ * @enum number
+ */
 export const TokenType = {
+  None: 0,
   Text: 1,
+  Comment: 2,
+  Whitespace: 3,
+  VariableName: 4,
+  Punctuation: 5,
+  Numeric: 6,
+  String: 7,
 }
 
 export const TokenMap = {
   [TokenType.Text]: 'Text',
+  [TokenType.Comment]: 'Comment',
+  [TokenType.Whitespace]: 'Whitespace',
+  [TokenType.VariableName]: 'VariableName',
+  [TokenType.Punctuation]: 'Punctuation',
+  [TokenType.Numeric]: 'Numeric',
+  [TokenType.String]: 'String',
 }
+
+const RE_LINE_COMMENT = /^#.*/s
+const RE_WHITESPACE = /^\s+/
+const RE_VARIABLE_NAME = /^[a-zA-Z\_\/\-\$][a-zA-Z\_\/\-\$#\d\-]*/
+const RE_PUNCTUATION = /^[:,;\{\}\[\]\.=\(\)<>\!\|\+\&\>\)]/
+const RE_NUMERIC =
+  /^((0(x|X)[0-9a-fA-F]*)|(([0-9]+\.?[0-9]*)|(\.[0-9]+))((e|E)(\+|-)?[0-9]+)?)\b/
+const RE_STRING_ESCAPE = /^\\./
+const RE_BACKSLASH_AT_END = /^\\$/
+const RE_ANYTHING = /^.+/s
+const RE_QUOTE_DOUBLE = /^"/
+const RE_STRING_DOUBLE_QUOTE_CONTENT = /^[^"\\]+/
 
 export const initialLineState = {
   state: 1,
@@ -18,11 +55,77 @@ export const hasArrayReturn = true
 
 /**
  * @param {string} line
+ * @param {any} lineState
  */
-export const tokenizeLine = (line) => {
-  const tokens = [TokenType.Text, line.length]
+export const tokenizeLine = (line, lineState) => {
+  let next = null
+  let index = 0
+  let tokens = []
+  let token = TokenType.None
+  let state = lineState.state
+  let stringEnd = lineState.stringEnd
+  const knownFunctionNames = new Set(lineState.knownFunctionNames)
+  while (index < line.length) {
+    const part = line.slice(index)
+    switch (state) {
+      case State.TopLevelContent:
+        if ((next = part.match(RE_WHITESPACE))) {
+          token = TokenType.Whitespace
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_PUNCTUATION))) {
+          token = TokenType.Punctuation
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_VARIABLE_NAME))) {
+          token = TokenType.VariableName
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_NUMERIC))) {
+          token = TokenType.Numeric
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_QUOTE_DOUBLE))) {
+          token = TokenType.Punctuation
+          state = State.InsideDoubleQuoteString
+        } else if ((next = part.match(RE_LINE_COMMENT))) {
+          token = TokenType.Comment
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_NUMERIC))) {
+          token = TokenType.Numeric
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_ANYTHING))) {
+          token = TokenType.Text
+          state = State.TopLevelContent
+        } else {
+          part //?
+          throw new Error('no')
+        }
+        break
+      case State.InsideDoubleQuoteString:
+        if ((next = part.match(RE_QUOTE_DOUBLE))) {
+          token = TokenType.Punctuation
+          state = State.TopLevelContent
+        } else if ((next = part.match(RE_STRING_DOUBLE_QUOTE_CONTENT))) {
+          token = TokenType.String
+          state = State.InsideDoubleQuoteString
+        } else if ((next = part.match(RE_STRING_ESCAPE))) {
+          token = TokenType.String
+          state = State.InsideDoubleQuoteString
+        } else if ((next = part.match(RE_BACKSLASH_AT_END))) {
+          token = TokenType.String
+          state = State.InsideDoubleQuoteString
+        } else {
+          throw new Error('no')
+        }
+        break
+      default:
+        throw new Error('no')
+    }
+    const tokenLength = next[0].length
+    index += tokenLength
+    tokens.push(token, tokenLength)
+  }
   return {
-    state: 1,
+    state,
     tokens,
+    knownFunctionNames,
+    stringEnd,
   }
 }
