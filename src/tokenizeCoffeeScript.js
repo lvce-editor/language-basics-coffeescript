ffe91faf0a9c609082bd55accbaaac01d78736e1
Le Vivilet <levivlet@pm.me>
2023-01-05 16:39:01 +0100

    fix: improve highlighting of undefined keyword

--- src/tokenizeCoffeeScript.js
+++ src/tokenizeCoffeeScript.js
@@ -111,6 +111,7 @@ export const tokenizeLine = (line, lineState) => {
             case 'off':
             case 'yes':
             case 'no':
+            case 'undefined':
               token = TokenType.LanguageConstant
               break
             case 'import':
